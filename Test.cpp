be9d0f2254b51ff1793d1eecfc5aafead4ba44fe
Matan Ben Nagar <matannagar@gmail.com>
2021-03-07 14:16:56 +0200

    added tests

--- Test.cpp
+++ Test.cpp
@@ -43,12 +43,77 @@ TEST_CASE("Good snowman code") {
     
 }
 
+TEST_CASE("Verify Digit by part") {
+    //HAT
+CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(21111111) == string("  ___  \n ..... \n (.,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(31111111) == string("   _   \n  /_\\  \n (.,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(41111111) == string("  ___  \n (_*_) \n (.,.) \n<( : )>\n ( : ) "   ));
+    //NOSE
+CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(12111111) == string("       \n _===_ \n (...) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(13111111) == string("       \n _===_ \n (._.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(14111111) == string("       \n _===_ \n (. .) \n<( : )>\n ( : ) "   ));
+    //Left Eye
+CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(11211111) == string("       \n _===_ \n (o,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(11311111) == string("       \n _===_ \n (O,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(11411111) == string("       \n _===_ \n (-,.) \n<( : )>\n ( : ) "   ));
+    //Right Eye
+CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(11121111) == string("       \n _===_ \n (.,o) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(11131111) == string("       \n _===_ \n (.,O) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(11141111) == string("       \n _===_ \n (.,-) \n<( : )>\n ( : ) "   ));
+    //Left Arm
+CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(11112111) == string("       \n _===_ \n (.,.) \n\( : )>\n ( : ) "   ));
+CHECK(snowman(11113111) == string("       \n _===_ \n (.,.) \n/( : )>\n ( : ) "   ));
+CHECK(snowman(11114111) == string("       \n _===_ \n (.,.) \n ( : )>\n ( : ) "   ));
+    //Right Arm
+CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(11111211) == string("       \n _===_ \n (.,.) \n<( : )/\n ( : ) "   ));
+CHECK(snowman(11111311) == string("       \n _===_ \n (.,.) \n<( : )\\n ( : ) "   ));
+CHECK(snowman(11111411) == string("       \n _===_ \n (.,.) \n<( : ) \n ( : ) "   ));
+    //TORSO
+CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(11111121) == string("       \n _===_ \n (.,.) \n<( : )>\n (] [) "   ));
+CHECK(snowman(11111131) == string("       \n _===_ \n (.,.) \n<( : )>\n (> <) "   ));
+CHECK(snowman(11111141) == string("       \n _===_ \n (.,.) \n<( : )>\n (   ) "   ));
+    //BASE
+CHECK(snowman(11111111) == string("       \n _===_ \n (.,.) \n<( : )>\n ( : ) "   ));
+CHECK(snowman(11111112) == string("       \n _===_ \n (.,.) \n<( : )>\n (" ") "   ));
+CHECK(snowman(11111113) == string("       \n _===_ \n (.,.) \n<( : )>\n (___) "   ));
+CHECK(snowman(11111114) == string("       \n _===_ \n (.,.) \n<( : )>\n (   ) "   ));
+
+}
+
 TEST_CASE("Illegal Digits") {
     CHECK_THROWS(snowman(55555555)); // digits are too big
     CHECK_THROWS(snowman(12341235));
     CHECK_THROWS(snowman(01231234));
-    CHECK_THROWS(snowman(-11111111));
+    CHECK_THROWS(snowman(-11111111)); //negative values
     CHECK_THROWS(snowman(00000000)); //no zeros allowd
+
+    //Illegal digit at every spot
+    CHECK_THROWS(snowman(01111111)); //no zeros allowd
+    CHECK_THROWS(snowman(10111111)); //no zeros allowd
+    CHECK_THROWS(snowman(11011111)); //no zeros allowd
+    CHECK_THROWS(snowman(11101111)); //no zeros allowd
+    CHECK_THROWS(snowman(11110111)); //no zeros allowd
+    CHECK_THROWS(snowman(11111011)); //no zeros allowd
+    CHECK_THROWS(snowman(11111101)); //no zeros allowd
+    CHECK_THROWS(snowman(11111110)); //no zeros allowd
+
+    //Illegal input
+    CHECK_THROWS(snowman(a1111111)); //no letters allowd
+    CHECK_THROWS(snowman(1a111111)); //no letters allowd
+    CHECK_THROWS(snowman(11a11111)); //no letters allowd
+    CHECK_THROWS(snowman(111a1111)); //no letters allowd
+    CHECK_THROWS(snowman(1111a111)); //no letters allowd
+    CHECK_THROWS(snowman(11111a11)); //no letters allowd
+    CHECK_THROWS(snowman(111111a1)); //no letters allowd
+    CHECK_THROWS(snowman(1111111a)); //no letters allowd
+    
 
     /* Add more checks here */
 }
